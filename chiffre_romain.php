e2008065c2976deafaddcb5b67456ca2ead90f99
Madické THIANE <madicke.thiane@gmail.com>
2021-06-13 12:28:34 +0200

    ajout du tableau de correspondance et fonction extraireChiffreRomain

--- chiffre_romain.php
+++ chiffre_romain.php
@@ -9,5 +9,16 @@ C => 100
 M => 1000
 */
 function conv(int $x) {
-if($x ){}
+$chiffreRomain = "";
+$correspondances=[["M",1000],["CM",900],["D",500],["CD|",400],["C",100],["XC",90],["L",50],["XL",40],["X",10],["V",5],["IV",4],["I",1]];
+
+function extraireChiffreRomain($lettre, $valeurLettre){
+        while ($x>=$valeurLettre){
+                $x-=$valeurLettre;
+                global $chiffreRomain += "$lettre";
+        }
+}
+foreach($correspondances as $correspondance){
+        extraireChiffreRomain($correspondance[0], $correspondance[1]);
+}
 }
